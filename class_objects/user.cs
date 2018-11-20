10e754456dc1c41715d4521be05c5fdb049ff84b
Jeremy Nguyen <nguyjere@oregonstate.edu>
2018-11-19 22:43:11 -0800

    Added Class Files
    Users, Courses, and Recipes

---
+++ class_objects/user.cs
@@ -0,0 +1,43 @@
+public class User
+{
+    public int id { get; }
+    public string Name { get; }
+    public string Status { get; }
+    public int EnrolledCourseId { get; }
+    public string CurrentCourseStatus { get; set; }
+
+    //Constructor
+    public User(string username)
+    {
+        if (validUsername(username) == true)
+        {
+            id = UserDN.getIdByName(username);
+            Name = UserDB.getNameById(id);
+            Status = UserDB.getStatusById(id);
+            if (isUserEnrolledInCourse(id) == true)
+            {
+                EnrolledCourseId = UserDB.getEnrolledCourseIdByUserId(id);
+                CurrentCourseStatus = UserDB.getCurrentCourseStatusByUserId(id);
+            }
+        }
+        else
+        {
+            //Throw error: invalid user
+        }
+    }
+
+    public void SwitchCourse(Course newCourse)
+    {
+        EnrolledCourse = newCourse.id;
+    }
+
+    public void UpdateUserStatus(int newStatus)
+    {
+        Status = newStatus;
+    }
+
+    public void UpdateCurrentCourseStatus(int newCourseStatus)
+    {
+        CurrentCourseStatus = newStatus;
+    }
+}
